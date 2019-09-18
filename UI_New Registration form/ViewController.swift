03e86b053775c58d473649fe37db4e1f685eec3b
COE-030 <coe-030@coe-030s-imac.local>
2019-09-18 15:31:59 +0530

    Registration form..

--- UI_New Registration form/ViewController.swift
+++ UI_New Registration form/ViewController.swift
@@ -8,18 +8,81 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController : UIViewController ,UIImagePickerControllerDelegate,UINavigationControllerDelegate
+{
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController){}
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any])
+    {
+        let image = info[UIImagePickerControllerOriginalImage] as!UIImage
+        imageview.image = image
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var imageview: UIImageView!
+    
+    var imagepickercontroller:UIImagePickerController!
+    
+    @IBAction func btnTakeImage(_ sender: Any)
+    {
+        imagepickercontroller = UIImagePickerController()
+        imagepickercontroller.delegate = self
+        imagepickercontroller.sourceType = .photoLibrary
+        self.present(imagepickercontroller, animated: true, completion: nil)
+    }
+    
+    
+    @IBOutlet weak var Username: UITextField!
+    @IBOutlet weak var Emailid: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    @IBOutlet weak var ConformPassword: UITextField!
+    
+    @IBOutlet weak var Gender: UISegmentedControl!
+    @IBAction func signUP(_ sender: Any) {
+        
+        print(self.Username.text!)
+        print(self.Emailid.text!)
+        print(self.Password.text!)
+        print(self.ConformPassword.text!)
+        print(self.Gender.selectedSegmentIndex)
+    }
 }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+    
+
 
